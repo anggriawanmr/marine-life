95234b2f0f6f0613240f3991858c5d106d2710cc
anggriawanmr <anggriawanmr@gmail.com>
2023-03-14 14:53:49 +0700

    Editing Sigin component :hammer:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-[#D7F3FF] border-4 border-yellow-300">
+    <nav className="sm:px-16 px-6 w-full flex items-center py-5 sticky top-0 z-20 bg-[#D7F3FF] border-4 border-yellow-300">
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto border-red-500 border-4">
         <Link to="/" className="flex items-center gap-2">
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="flex justify-center items-center min-w-full h-[87vh] bg-slate-50">
+      <div className="flex items-center rounded-2xl shadow-lg w-[60%] h-[80%] bg-[#fff]">
+        <div className="flex-1 flex flex-column items-center justify-center flex-col">
+          <div>Google</div>
+          <div>Facebook</div>
+          <div>Github</div>
+        </div>
+        <div className="h-[100%] flex items-center justify-center">
+          <div className="w-[0.1rem] h-[60%] bg-slate-300 absolute m-auto"></div>
+          <div className="rounded-full border-2 border-slate-300 flex items-center justify-center p-2 font-bold bg-[#fff] z-10">
+            OR
+          </div>
+        </div>
+        <div className="flex-1 flex flex-column items-center justify-center flex-col">
+          <input type="text" placeholder="Username"></input>
+          <input type="text" placeholder="Password"></input>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
